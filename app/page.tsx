ed63142cf414728e6f8e7b1ab7ddafd965fc567f
HARUKA <haruka.music.style@icloud.com>
2025-09-24 12:02:22 +0900

    fitst commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,21 @@
+"use client";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage/Home";
+// import SchedulePage from "./page/SchedulePage/Schedule";
+// import BlogPage from "./page/BlogPage/Blog";
+// import ContactPage from "./page/ContactPage/Contact";
+
 export default function Home() {
-  return <></>;
+  return (
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          {/* <Route path="/Schedule" element={<SchedulePage />} />
+          <Route path="/blog" element={<BlogPage />} />
+          <Route path="/contact" element={<ContactPage />} /> */}
+        </Routes>
+      </div>
+    </Router>
+  );
 }
